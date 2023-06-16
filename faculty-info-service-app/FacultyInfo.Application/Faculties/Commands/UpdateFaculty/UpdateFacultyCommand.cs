05d05574297e4d65e11ebf36715911c9e265b48e
milekajtez <milekajtez@gmail.com>
2023-06-17 00:07:56 +0200

    Add logic for editing faculty

---
+++ faculty-info-service-app/FacultyInfo.Application/Faculties/Commands/UpdateFaculty/UpdateFacultyCommand.cs
@@ -0,0 +1,23 @@
+﻿using FacultyInfo.Domain.Dtos.Faculty;
+using MediatR;
+using System.ComponentModel.DataAnnotations;
+
+namespace FacultyInfo.Application.Faculties.Commands.UpdateFaculty
+{
+    public class UpdateFacultyCommand : IRequest<FacultyDto>
+    {
+        [Required]
+        public Guid Id { get; set; }
+        
+        [StringLength(10)]
+        public string Tin { get; set; }
+
+        [MaxLength(200)]
+        public string Name { get; set; }
+
+        [MaxLength(500)]
+        public string Description { get; set; }
+
+        public string Location { get; set; }
+    }
+}
---
+++ faculty-info-service-app/FacultyInfo.Application/Faculties/Commands/UpdateFaculty/UpdateFacultyCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using FacultyInfo.Application.Helpers.Error;
+using FacultyInfo.Domain.Abstractions.UnitOfWork;
+using FacultyInfo.Domain.Dtos.Faculty;
+using FacultyInfo.Domain.Enums.ErrorMessage;
+using FacultyInfo.Domain.Exceptions;
+using FacultyInfo.Domain.Models;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace FacultyInfo.Application.Faculties.Commands.UpdateFaculty
+{
+    public class UpdateFacultyCommandHandler : IRequestHandler<UpdateFacultyCommand, FacultyDto>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public UpdateFacultyCommandHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+
+        }
+        public async Task<FacultyDto> Handle(UpdateFacultyCommand request, CancellationToken cancellationToken)
+        {
+            var faculty = await _unitOfWork.FacultyRepository
+                .Find(e => e.Id == request.Id)
+                .FirstOrDefaultAsync(cancellationToken) ?? 
+                throw new NotFoundException(
+                    ErrorMessage.GenerateErrorMessage(ErrorMessageType.FacultyHasNotFound, new string[1] { request.Id.ToString() }));
+
+            faculty = Faculty.Update(faculty, request.Tin, request.Name, request.Description, request.Location);
+
+            await _unitOfWork.CompleteAsync();
+
+            return _mapper.Map<FacultyDto>(faculty);
+        }
+    }
+}
