5517974a4571f359b803e42f3ca14bf34e9bc898
milekajtez <milekajtez@gmail.com>
2023-09-29 07:33:36 +0200

    Add logic for getting all faculty admins

---
+++ faculty-info-service-app/FacultyInfo.Application/FacultyAdmins/Queries/GetAllFacultyAdmins/GetAllFacultyAdminsQuery.cs
@@ -0,0 +1,6 @@
+﻿using FacultyInfo.Domain.Dtos.FacultyAdmin;
+using MediatR;
+
+namespace FacultyInfo.Application.FacultyAdmins.Queries.GetAllFacultyAdmins;
+
+public sealed record GetAllFacultyAdminsQuery() : IRequest<IEnumerable<FacultyAdminDto>>;
